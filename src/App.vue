7ce8abcc02f1abd7dc8631e163c868b87fc99b16
mbpxl <esscof1e@gmail.com>
2024-09-23 12:32:41 +0300

    initialized app

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import Navbar from './components/Navbar.vue';
+  import Hero from './components/Hero.vue';
+</script>
+
+<template>
+  <Navbar/>
+  <Hero/>
+</template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="bg-green-700 py-20 mb-4">
+      <div
+        class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+      >
+        <div class="text-center">
+          <h1
+            class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+          >
+            Become a Vue Dev
+          </h1>
+          <p class="my-4 text-xl text-white">
+            Find the Vue job that fits your skills and needs
+          </p>
+        </div>
+      </div>
+    </section>
+</template>
