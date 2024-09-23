03f8a7f9bef77f2b50e20ae97cc0000bc9247997
dirtyb1rd <yeskov777.888@gmail.com>
2024-09-23 22:54:21 +0300

    feature(JobListing.vue) add simple JSON fetch component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-  import Navbar from './components/Navbar.vue';
-  import Hero from './components/Hero.vue';
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
-  <Navbar/>
-  <Hero/>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings />
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div v-for="job in jobs" :key="job.id">
+          {{ job.title }}
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import jobData from "@/data/jobs.json"
+import { ref } from 'vue'
+
+const jobs = ref(jobData);
+</script>
